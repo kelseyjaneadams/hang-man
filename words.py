3447a207eb7819ece0787d6eff5cb1d093b53236
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-08-17 13:29:12 +0000

    Import word list into main file.

---
+++ words.py
@@ -0,0 +1,43 @@
+# Here are the list of words for each level of the Hangman game.
+
+easy_words = [
+    "apple", "bread", "chair", "dance", "eagle",
+    "field", "grass", "house", "jelly", "knife",
+    "lemon", "magic", "nurse", "ocean", "plant",
+    "quilt", "river", "stone", "table", "umbra",
+    "voice", "water", "xylo", "youth", "zebra",
+    "brain", "cloud", "drain", "flame", "globe",
+    "heart", "index", "joker", "kneel", "liver",
+    "mouse", "nerve", "otter", "piano", "queen",
+    "robot", "shark", "tiger", "ultra", "vivid",
+    "whale", "xenon", "yield", "zippy", "beach"
+]
+
+medium_words = [
+    "absolute", "advisory", "ancestor", "backpack", "birthday",
+    "blessing", "boundary", "building", "calendar", "cellular",
+    "circular", "compared", "creation", "database", "decision",
+    "describe", "dynamics", "elevator", "equation", "feedback",
+    "festival", "firewall", "forecast", "function", "gathered",
+    "graphics", "guidance", "handover", "hospital", "illusion",
+    "industry", "interest", "keyboard", "language", "laughter",
+    "magazine", "marathon", "medicine", "movement", "narrator",
+    "networks", "overcome", "personal", "platform", "possible",
+    "presence", "producer", "property", "protocol", "recovery"
+]
+
+hard_words = [
+    "aftershock", "backstroke", "blacksmith", "brainstorm",
+    "butterflies", "chivalrous", "chromosome", "crossroads",
+    "daydreamer", "doppelganger", "earthquake", "exoskeleton",
+    "fictionist", "frostbound", "gallivants", "goosebumps",
+    "halcyonian", "horsepower", "jellybeans", "lumberjack",
+    "moonflower", "mysterious", "nightscape", "nightshade",
+    "outlandish", "peppermint", "quarantine", "raconteurs",
+    "recharging", "refraction", "revolution", "riverfront",
+    "sandcastle", "schoolmate", "seclusion", "serpentine",
+    "skyscraper", "spacecraft", "storyboard", "thunderous",
+    "travellers", "treasures", "turbulence", "underneath",
+    "vagabondish", "ventriloquy", "watercraft", "whirlwinds",
+    "wonderland", "zigzagging"
+]
